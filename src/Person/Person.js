cbf1921afc74abd572536abeeedc223efd0efb19
Sharad Shinde <shindesharad71@gmail.com>
2020-10-03 14:41:47 +0530

    Added: Props Children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>Person Name: {props.name} and Age: {props.age}</p>
-}
+	return (
+		<div>
+			<p>
+				Person Name: {props.name} and Age: {props.age}
+            </p>
+            <p>{props.children}</p>
+		</div>
+	);
+};
 
 export default person;
