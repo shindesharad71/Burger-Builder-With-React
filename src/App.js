2cb63c4a9659a62069446009591ae210a93a7b6e
Sharad Shinde <shindesharad71@gmail.com>
2020-10-03 15:04:01 +0530

    Added: Props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-	return (
-		<div className="App">
-      <h1>This is React App</h1>
-      <Person name="Sharad" age="26"/>
-      <Person name="Avi" age="28">Hobbies: Watching Anime</Person>
-      <Person name="Jabya" age="24"/>
-		</div>
-	);
+class App extends Component {
+  
+  state = {
+    persons: [
+      {name: 'Sharad', age: 26},
+      {name: 'Avi', age: 28},
+      {name: 'Jabya', age: 24}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>This is React App</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].name}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Watching Anime</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].name}/>
+      </div>
+    );
+  }
 }
 
 export default App;
