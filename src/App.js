9dd1deaef98783fcffa89d83e1c8de3185febe31
Sharad Shinde <shindesharad71@gmail.com>
2020-10-03 15:18:35 +0530

    Added: Modify State Using setState

--- src/App.js
+++ src/App.js
@@ -12,10 +12,21 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name: 'Sharad Shinde', age: 26},
+        {name: 'Avinash Mali', age: 28},
+        {name: 'Aditya Madole', age: 24}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>This is React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].name}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Watching Anime</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].name}/>
