7f75724923e85619e0dc696f2dc4b26961097e54
Sharad Shinde <shindesharad71@gmail.com>
2020-10-03 14:31:13 +0530

    Added: New Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
       <h1>This is React App</h1>
-    </div>
-  );
+      <Person name="Sharad" age="26"/>
+      <Person name="Avi" age="28">Hobbies: Anime</Person>
+      <Person name="Jabya" age="24"/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>Time is {Date.now()}</p>
+}
+
+export default person;
