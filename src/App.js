45969e045c77b1831e0fd92c07e2ad5e4eacd75d
Sharad Shinde <shindesharad71@gmail.com>
2020-10-04 12:29:01 +0530

    Added: Dynamic Rendering

--- src/App.js
+++ src/App.js
@@ -42,21 +42,14 @@ class App extends Component {
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					<Person
-						name={this.state.persons[0].name}
-						age={this.state.persons[0].age}
-						changed={this.nameChangedHandler}
-					/>
-					<Person
-						name={this.state.persons[1].name}
-						age={this.state.persons[1].age}
-					>
-						Hobbies: Watching Anime
-					</Person>
-					<Person
-						name={this.state.persons[2].name}
-						age={this.state.persons[2].age}
-					/>
+					{this.state.persons.map((person, i) => (
+						<Person
+							name={person.name}
+              age={person.age}
+              key={i}
+							changed={this.nameChangedHandler}
+						/>
+					))}
 				</div>
 			);
 		}
