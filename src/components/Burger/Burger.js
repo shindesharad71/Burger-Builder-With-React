f74780f9b4d940c7ee12222bba4e26e194fcd91f
Sharad Shinde <shindesharad71@gmail.com>
2020-10-08 18:43:09 +0530

    Added: Render Ingredient Dynamicly

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.module.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients).map(
+		(igKey) => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		}
+	);
+
 	return (
 		<div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="cheese" />
+			<BurgerIngredient type="bread-top" />
+			{transformedIngredients}
 			<BurgerIngredient type="bread-bottom" />
 		</div>
 	);
