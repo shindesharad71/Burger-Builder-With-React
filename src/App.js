cabc1900ceed4050d097187bc2962e51bce364e9
Sharad Shinde <shindesharad71@gmail.com>
2020-10-08 16:53:00 +0530

    Added: BurgerBuilder Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <h1>This is Working</h1>
+        <BurgerBuilder/>
       </Layout>
     );
   }
