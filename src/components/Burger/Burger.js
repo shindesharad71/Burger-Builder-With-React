64817870e4ba8a974095096949b25cd93bdf8d15
Sharad Shinde <shindesharad71@gmail.com>
2020-10-08 19:00:31 +0530

    Added: Empty State Check

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,18 @@ import classes from './Burger.module.scss';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-	const transformedIngredients = Object.keys(props.ingredients).map(
+	let transformedIngredients = Object.keys(props.ingredients).map(
 		(igKey) => {
 			return [...Array(props.ingredients[igKey])].map((_, i) => {
 				return <BurgerIngredient key={igKey + i} type={igKey} />;
 			});
 		}
-	);
+    );
+    transformedIngredients = transformedIngredients.flat();
+    
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
 
 	return (
 		<div className={classes.Burger}>
