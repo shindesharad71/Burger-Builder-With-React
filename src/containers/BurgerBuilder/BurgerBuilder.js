0046115168bbaf0c51075dd0e27b013f815e7643
Sharad Shinde <shindesharad71@gmail.com>
2020-10-10 10:50:15 +0530

    Added: Disalbe Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,7 +38,12 @@ class BurgerBuilder extends Component {
 	};
 
 	removeIngredientHandler = (type) => {
-		const oldCount = this.state.ingredients[type];
+        const oldCount = this.state.ingredients[type];
+        
+        if(oldCount <= 0) {
+            return;
+        }
+
 		const updatedCount = oldCount - 1;
 		const updatedIngredients = {
 			...this.state.ingredients,
@@ -54,12 +59,20 @@ class BurgerBuilder extends Component {
 	};
 
 	render() {
+
+        const disabledInfo = {...this.state.ingredients};
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
-					ingredientRemoved={this.removeIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
 				/>
 			</Aux>
 		);
