dda043c0283541fb8417956932064d49b1cc2085
Sharad Shinde <shindesharad71@gmail.com>
2020-10-16 12:44:09 +0530

    Improved Perfomamce

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,6 +4,11 @@ import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.show !== this.props.show;
+	}
+
 	render() {
 		return (
 			<Aux>
