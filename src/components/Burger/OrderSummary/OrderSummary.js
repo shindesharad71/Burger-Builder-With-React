967d4ea1f0657299f4cce315f198cbe0805c2c2a
Sharad Shinde <shindesharad71@gmail.com>
2020-10-16 12:40:53 +0530

    Updated: Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,11 @@ import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+
+	componentDidUpdate() {
+		
+	}
+
 	render() {
 
 		const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => (
