a65bd00efd20d3a45d5716027eca3d318727a0b8
Sharad Shinde <sharad.shinde@coditas.com>
2020-10-23 15:33:12 +0530

    Added: Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
+import { Route, Switch } from 'react-router-dom';
+
 class App extends Component {
 	render() {
 		return (
 			<Layout>
-				<BurgerBuilder />
-				<Checkout />
+				<Switch>
+					<Route path="/" exact component={BurgerBuilder} />
+					<Route path="/checkout" component={Checkout} />
+				</Switch>
 			</Layout>
 		);
 	}
