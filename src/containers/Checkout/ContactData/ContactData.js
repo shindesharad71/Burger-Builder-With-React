e5b88ee00134659503c02f398115abfaa48d0d2c
Sharad Shinde <sharad.shinde@coditas.com>
2020-10-24 13:42:50 +0530

    Updated: Form Component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.scss';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 	state = {
@@ -47,10 +48,10 @@ class ContactData extends Component {
 	render() {
 		let form = (
 			<form>
-				<input type="text" name="name" placeholder="Your name" />
-				<input type="email" name="email" placeholder="Your email" />
-				<input type="text" name="street" placeholder="Your street" />
-				<input
+				<Input inputtype="input" type="text" name="name" placeholder="Your name" />
+				<Input inputtype="input" type="email" name="email" placeholder="Your email" />
+				<Input inputtype="input" type="text" name="street" placeholder="Your street" />
+				<Input inputtype="input"
 					type="text"
 					name="postal"
 					placeholder="Your postal code"
