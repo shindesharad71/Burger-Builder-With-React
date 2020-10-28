abcf8e1314715be086956e84d22f28a6a80ec2c5
Sharad Shinde <sharad.shinde@coditas.com>
2020-10-28 16:43:22 +0530

    Added: Store

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
 import { HashRouter } from 'react-router-dom';
 
+const store = createStore(reducer);
+
 const app = (
-	<HashRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</HashRouter>
+	<Provider store={store}>
+		<HashRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</HashRouter>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
