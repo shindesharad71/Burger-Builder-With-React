05158562ade9fdb57e58d428982b0c2158fbd7f5
Sharad Shinde <sharad.shinde@coditas.com>
2020-12-13 13:33:56 +0530

    Added: Route Guards

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from './store/actions'
+import * as actions from './store/actions';
 
 class App extends Component {
 	componentDidMount() {
@@ -15,24 +15,40 @@ class App extends Component {
 	}
 
 	render() {
-		return (
-			<Layout>
-				<Switch>
-				<Route path="/orders" component={Orders} />
-				<Route path="/checkout" component={Checkout} />
+		let routes = (
+			<Switch>
 				<Route path="/auth" component={Auth} />
-				<Route path="/logout" component={Logout} />
 				<Route path="/" exact component={BurgerBuilder} />
-				</Switch>
-			</Layout>
+				<Redirect to="/" />
+			</Switch>
 		);
+
+		if (this.props.isAuthenticated) {
+			routes = (
+				<Switch>
+					<Route path="/orders" component={Orders} />
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/logout" component={Logout} />
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</Switch>
+			);
+		}
+
+		return <Layout>{routes}</Layout>;
 	}
 }
 
+const mapStateToProps = (state) => {
+	return {
+		isAuthenticated: state.auth.token !== null,
+	};
+};
+
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onTrySignup: () => dispatch(actions.authCheckState())
+		onTrySignup: () => dispatch(actions.authCheckState()),
 	};
-}
+};
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
